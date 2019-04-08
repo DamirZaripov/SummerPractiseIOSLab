4b199f3da4838f983f66454dac86baa67d951b26
Дамир Зарипов <damir@MacBook-Pro-Damir.local>
2019-04-08 19:05:02 +0300

    Initial commit

---
+++ SummerPractiseIOSLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SummerPractiseIOSLab
+//
+//  Created by Дамир Зарипов on 08/04/2019.
+//  Copyright © 2019 itisIOSLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
