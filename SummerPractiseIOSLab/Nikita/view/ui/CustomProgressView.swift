1ab834617ecd3846cad200af13a9c3692e691f0e
enoxus <reqenoxus@gmail.com>
2019-04-09 21:02:11 +0300

    replaced the default progressview with a thicker one

---
+++ SummerPractiseIOSLab/Nikita/view/ui/CustomProgressView.swift
@@ -0,0 +1,24 @@
+//
+//  CustomProgressView.swift
+//  SummerPractiseIOSLab
+//
+//  Created by Enoxus on 09/04/2019.
+//  Copyright © 2019 itisIOSLab. All rights reserved.
+//
+
+import UIKit
+
+class CustomProgressView: UIProgressView {
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let maskLayerPath = UIBezierPath(roundedRect: bounds, cornerRadius: 4.0)
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = self.bounds
+        maskLayer.path = maskLayerPath.cgPath
+        layer.mask = maskLayer
+        
+    }
+
+}
