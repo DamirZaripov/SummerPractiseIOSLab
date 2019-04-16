b64f311c6c55b90fd90fd701639b3694484464bb
enoxus <reqenoxus@gmail.com>
2019-04-16 13:12:55 +0300

    transparent navigation controller implementation

--- SummerPractiseIOSLab/Nikita/view/MainView.swift
+++ SummerPractiseIOSLab/Nikita/view/MainView.swift
@@ -12,6 +12,16 @@ class MainView: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var highscoreLabel: UILabel!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = .clear
+        self.navigationController?.navigationBar.backgroundColor = .clear
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         highscoreLabel.text = "Highscore: " + String(UserDefaults.standard.integer(forKey: "highscore"))
