9cee4a24e7a6d741d690716f3990f09a9b91cc4d
AlIld <49414605+AlIld@users.noreply.github.com>
2019-04-26 00:38:17 +0300

    Update AppDelegate.swift

--- SummerPractiseIOSLab/Helpers/AppDelegate.swift
+++ SummerPractiseIOSLab/Helpers/AppDelegate.swift
@@ -1,46 +1,27 @@
 //
-// AppDelegate.swift
-// notificationTest
+//  AppDelegate.swift
+//  SummerPractiseIOSLab
 //
-// Created by itisioslab on 24/04/2019.
-// Copyright © 2019 itisioslab. All rights reserved.
+//  Created by Дамир Зарипов on 08/04/2019.
+//  Copyright © 2019 itisIOSLab. All rights reserved.
 //
 
 import UIKit
-//import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate//, UNUserNotificationCenterDelegate
-   {
+class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
- /*   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        let center = UNUserNotificationCenter.current()
-        
-        let options: UNAuthorizationOptions = [.sound, .alert]
-        
-        center.requestAuthorization(options: options){(granted, error) in
-            if error != nil {
-                print(error)
-            }
-        }
-        
-        center.delegate = self
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
         return true
     }
     
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification:
-        UNNotification, withCompletionHandler completionHandler: @escaping
-        (UNNotificationPresentationOptions) -> Void){
-        completionHandler([.alert, .badge, .sound])
-    }*/
-    
-    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -49,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate//, UNUserNotificationCente
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
     
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -59,6 +40,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate//, UNUserNotificationCente
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
     
 }
 
